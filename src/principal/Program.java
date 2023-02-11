c55aa12850028e4761c96a29156e804872314f28
Phaser490 <marcellongois@gmail.com>
2023-02-11 14:25:35 -0300

    first commit

---
+++ src/principal/Program.java
@@ -0,0 +1,12 @@
+package principal;
+
+import controllers.PessoaController;
+
+public class Program {
+	public static void main(String[] args) {
+
+		PessoaController pessoaController = new PessoaController();
+		pessoaController.cadastrarPessoa();
+
+	}
+}
---
+++ src/sql/script.sql
@@ -0,0 +1,5 @@
+creat table pessoa(
+ idpessoa          serial          primary key,
+ nome              varchar(150)     not null
+ email             varchar(100)     not null
+ );
